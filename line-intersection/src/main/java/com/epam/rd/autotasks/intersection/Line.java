8155f926742aa1f76bf65e0c7934c915cc39e750
Technologyk <58766721+Ruslan10-h@users.noreply.github.com>
2022-03-25 21:31:09 +0200

    Add files via upload

---
+++ line-intersection/src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -0,0 +1,23 @@
+package com.epam.rd.autotasks.intersection;
+
+public class Line {
+    private final int k;
+    private final int b;
+
+    public Line(int k, int b) {
+        this.k = k;
+        this.b = b;
+    }
+
+    public Point intersection(Line other) {
+        int deltaB = this.b - other.b;
+        int deltaK = other.k - this.k;
+        if (deltaK == 0)
+            return null;
+        else {
+            int x = deltaB/deltaK;
+            return new Point(x, this.k*x + this.b );
+        }
+    }
+
+}
